8a46ba2043f26648f5b14c78a27b21790577e816
Student-ISTU21 <81580281+Student-ISTU21@users.noreply.github.com>
2021-03-30 00:27:48 +0800

    The first task

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
-# создаём массив.
-# Он нужен, чтобы не вычислять одни и те же значения по несколько раз
-chisla = [0] * 100
+from sys import stdin
 
+first = []
+# первый список
+for s in stdin:
+    # если строка непустая, то присоединяем ее содержимое к кортежу, иначе - выходим из циклв
+    if len(s) > 1:
+        first.append(tuple(map(int, s.split())))
+    else:
+        break
+# количество следующих списков можно не запоминать - нам нужен только самый первый подходящий по условия список
+input()
 
-def fiboncci(n):
-    # если ячейка массива пустая, то вычисляем значение
-    # иначе - сразу переходим к return
-    if chisla[n] == 0:
-        if n == 1 or n == 2:
-            chisla[n] = 1
+# функция для проверки соответствия списка заданным условиям
+def check(a, b):
+    # проверка на схожесть формы строк списков
+    if len(a) != len(b):
+        return False
+    # проверка на то, отличаются ли соответствующие элементы друг на друга на 7
+    for i, j in zip(a, b):
+        if not all(abs(x - y) == 7 for x, y in zip(i, j)):
+            return False
+    return True
+
+# ищем нужный список
+second = []
+for s in stdin:
+    # если строка непустая, то присоединяем ее содержимое к кортежу second
+    # иначе - проверяем кортеж на соответствие условиям
+    if len(s) > 1:
+        second.append(tuple(map(int, s.split())))
+    else:
+        result = check(first, second)
+        # если second соответствует условиям, то выходим из цикла
+        # иначе - обнуляем second
+        if result:
+            break
         else:
-            chisla[n] = fibo(n - 1) + fibo(n - 2)
-    return chisla[n]
+            second = []
+else:
+    result = check(first, second)
 
-
-n = int(input())
-print(fiboncci(n))
+if result:
+    for row in second:
+        print('Вывод:')
+        print(*row)
+else:
+    print('Матрица уникальна')
